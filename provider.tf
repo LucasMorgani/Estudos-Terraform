d2a8e8abf1ace92de0949eccaa6dafef8bf43917
LucasMorgani <morganis1411@gmail.com>
2025-08-18 17:50:34 -0300

    fix: lint

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.9.0"
     }
   }
   backend "s3" { #Migrando o tfstate para um bucket s3
     bucket = "skylabs-comunidadedevops"
-    key = "dev/terraform.tfstate"
+    key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
 }
