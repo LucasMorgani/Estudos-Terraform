bcf5f236ec472e9275dea4df357fa9443776e3bb
LucasMorgani <morganis1411@gmail.com>
2025-08-18 17:20:25 -0300

    feat: passando o tfstate para um bucket

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "6.9.0"
     }
   }
+  backend "s3" { #Migrando o tfstate para um bucket s3
+    bucket = "skylabs-comunidadedevops"
+    key = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
