874f84752d13459ae397fc06e5df4bdfcdf34486
LucasMorgani <morganis1411@gmail.com>
2025-08-19 10:02:04 -0300

    feat: decouple tags

--- public.tf
+++ public.tf
@@ -1,21 +1,45 @@
+#Criando uma subnet - us-east-1a
 resource "aws_subnet" "eks_subnet_1a" {
-  vpc_id                  = aws_vpc.eks_vpc.id               #Pegando o id da vpc como atributo
-  cidr_block              = cidrsubnet(var.cidr_block, 8, 1) #Gerando o CIDR 10.0.1.0/24
-  availability_zone       = "us-east-1a"
+
+  #Dados da subnet
+  vpc_id                  = aws_vpc.eks_vpc.id                   #Pegando o id da vpc como atributo
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)     #Gerando o CIDR 10.0.1.0/24
+  availability_zone       = "${data.aws_region.current.region}a" #Pegando a região atual com o data source (region.tf)
   map_public_ip_on_launch = true
+  #----
 
-  tags = {
-    Name = "comunidadedevops-subnet-1a"
-  }
+  #Declarando tags mescladas, entre reutilizaveis (locals.tf) e tags especificas locais
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "comunidadedevops-subnet-1a",
+      "kubernetes.io/role/elb" = 1 #Passando a tag de subnet publica
+    }
+  )
+  #----
+
 }
+#----  
 
+#Criando uma subnet - us-east-1b
 resource "aws_subnet" "eks_subnet_1b" {
-  vpc_id                  = aws_vpc.eks_vpc.id               #Pegando o id da vpc como atributo
-  cidr_block              = cidrsubnet(var.cidr_block, 8, 2) #Gerando o CIDR 10.0.2.0/24
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
 
-  tags = {
-    Name = "comunidadedevops-subnet-1b"
-  }
+  #Dados da subnet
+  vpc_id                  = aws_vpc.eks_vpc.id                   #Pegando o id da vpc como atributo
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)     #Gerando o CIDR 10.0.2.0/24
+  availability_zone       = "${data.aws_region.current.region}b" #Pegando a região atual com o data source (region.tf)
+  map_public_ip_on_launch = true
+  #----
+
+  #Declarando tags mescladas, entre reutilizaveis (locals.tf) e tags especificas locais
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "comunidadedevops-subnet-1b",
+      "kubernetes.io/role/elb" = 1 #Passando a tag de subnet publica
+    }
+  )
+  #----
+
 }
+#----
--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,17 @@
 resource "aws_vpc" "eks_vpc" {
+  #Dados dos resources
   cidr_block           = var.cidr_block # Puxa o valor informado em variables.tf
   enable_dns_support   = true
   enable_dns_hostnames = true
+  #----
 
-  tags = {
-    Name = "comunidadedevops-vpc"
-  }
+  #Declarando tags mescladas, entre reutilizaveis (locals.tf) e tags especificas locais
+  tags = merge(
+    local.tags, #Passando variaveis reutilizaveis salvas em locals.tf
+    {
+      Name = "comunidadedevops-vpc" #Passando a tag especifica local
+    }
+  )
+  #----
+
 }
