777859ba95ae8dca0549645ed3e192025741be6a
LucasMorgani <morganis1411@gmail.com>
2025-08-19 14:07:56 -0300

    feat: adding private subnets

---
+++ private.tf
@@ -0,0 +1,37 @@
+#Criando subnet privada - us-east-1a
+resource "aws_subnet" "eks_subnet_priv_1a" {
+  #Dados da subnet
+  vpc_id            = aws_vpc.eks_vpc.id                   #Pegando o id da vpc como atributo
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 3)     #Gerando o CIDR 10.0.3.0/24
+  availability_zone = "${data.aws_region.current.region}a" #Pegando a região atual com o data source (region.tf)
+  #----
+  #Declarando tags mescladas, entre reutilizaveis (locals.tf) e tags especificas locais
+  tags = merge(
+    local.tags,
+    {
+      Name                              = "comunidadedevops-priv-subnet-1a"
+      "kubernetes.io/role/internal-elb" = 1
+    }
+  )
+  #----
+}
+#----
+
+#Criando subnet privada - us-east-1b
+resource "aws_subnet" "eks_subnet_priv_1b" {
+  #Dados da subnet
+  vpc_id            = aws_vpc.eks_vpc.id                   #Pegando o id da vpc como atributo
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 4)     #Gerando o CIDR 10.0.4.0/24
+  availability_zone = "${data.aws_region.current.region}b" #Pegando a região atual com o data source (region.tf)
+  #----
+  #Declarando tags mescladas, entre reutilizaveis (locals.tf) e tags especificas locais
+  tags = merge(
+    local.tags,
+    {
+      Name                              = "comunidadedevops-priv-subnet-1b"
+      "kubernetes.io/role/internal-elb" = 1
+    }
+  )
+  #----
+}
+#----
--- public.tf
+++ public.tf
@@ -1,45 +1,39 @@
-#Criando uma subnet - us-east-1a
-resource "aws_subnet" "eks_subnet_1a" {
-
+#Criando subnet publica - us-east-1a
+resource "aws_subnet" "eks_subnet_pub_1a" {
   #Dados da subnet
   vpc_id                  = aws_vpc.eks_vpc.id                   #Pegando o id da vpc como atributo
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)     #Gerando o CIDR 10.0.1.0/24
   availability_zone       = "${data.aws_region.current.region}a" #Pegando a região atual com o data source (region.tf)
   map_public_ip_on_launch = true
   #----
-
   #Declarando tags mescladas, entre reutilizaveis (locals.tf) e tags especificas locais
   tags = merge(
     local.tags,
     {
-      Name                     = "comunidadedevops-subnet-1a",
+      Name                     = "comunidadedevops-pub-subnet-1a",
       "kubernetes.io/role/elb" = 1 #Passando a tag de subnet publica
     }
   )
   #----
-
 }
-#----  
+#----
 
 #Criando uma subnet - us-east-1b
-resource "aws_subnet" "eks_subnet_1b" {
-
+resource "aws_subnet" "eks_subnet_pub_1b" {
   #Dados da subnet
   vpc_id                  = aws_vpc.eks_vpc.id                   #Pegando o id da vpc como atributo
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)     #Gerando o CIDR 10.0.2.0/24
   availability_zone       = "${data.aws_region.current.region}b" #Pegando a região atual com o data source (region.tf)
   map_public_ip_on_launch = true
   #----
-
   #Declarando tags mescladas, entre reutilizaveis (locals.tf) e tags especificas locais
   tags = merge(
     local.tags,
     {
-      Name                     = "comunidadedevops-subnet-1b",
+      Name                     = "comunidadedevops-pub-subnet-1b",
       "kubernetes.io/role/elb" = 1 #Passando a tag de subnet publica
     }
   )
   #----
-
 }
 #----
